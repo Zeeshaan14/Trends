6eeb90a68f351ec315bd6ff564952ae288914e1d
Zeeshaan14 <Zeeshaan356@gmail.com>
2026-01-16 13:39:19 +0530

    feat: Initialize Express server with core middleware, API routes, and global error handling.

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(cors({
   origin: [
     "http://localhost:3000",
-    "https://trends-kappa.vercel.app"
+    "https://nu-jerseys.vercel.app"
   ],
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
   credentials: true
